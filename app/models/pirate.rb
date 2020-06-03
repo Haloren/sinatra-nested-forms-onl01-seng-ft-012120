2eef6fd557dc7057148e1f96a7d2f0e6091a5bca
Haloren <mckennakyler@gmail.com>
2020-06-02 17:56:52 -0700

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+
+    attr_accessor :name, :weight, :height
+
+    @@all = []
+
+    def initialize(attributes)
+        @name = attributes[:name]
+        @height = attributes[:height]
+        @weight = attributes[:weight]
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,24 @@
 class Ship
+
+    attr_accessor :name, :type, :booty
+
+    @@all = []
+
+    def initialize(attributes)
+        @name = attributes[:name]
+        @type = attributes[:type]
+        @booty = attributes[:booty]
+
+        @@all << self
+
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.destroy
+        @@all.destroy
+    end
+
 end
